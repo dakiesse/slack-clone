fd1a14961230988f1b1e11e8daa9bc7413c413bd
Danil Chekalin <dakiesse@gmail.com>
2019-01-04 02:31:58 +0200

    add simple validation of the register page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -4,7 +4,43 @@ import { Link } from 'react-router-dom'
 import firebase from '../../firebase'
 
 export default class Register extends Component {
-  state = {}
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    errors: [],
+  }
+
+  isFormValid = () => {
+    let error
+
+    if (this.isFormEmpty()) {
+      error = { message: 'Fill in all fields' }
+    } else if (!this.isPasswordValid()) {
+      error = { message: 'Password is invalid' }
+    }
+
+    if (error) {
+      this.setState({ errors: [error] })
+      return false
+    }
+
+    this.setState({ errors: [] })
+    return true
+  }
+
+  isFormEmpty = () => {
+    const { username, email, password, passwordConfirmation } = this.state
+
+    return !username.length || !email.length || !password.length || !passwordConfirmation.length
+  }
+
+  isPasswordValid = () => {
+    const { password, passwordConfirmation } = this.state
+
+    return password.length < 6 ? false : password === passwordConfirmation
+  }
 
   handlerChange = (e) => {
     const { name, value } = e.target
@@ -17,6 +53,8 @@ export default class Register extends Component {
 
     e.preventDefault()
 
+    if (!this.isFormValid()) return false
+
     try {
       const createdUser = await firebase.auth().createUserWithEmailAndPassword(email, password)
       console.log(createdUser)
@@ -25,7 +63,7 @@ export default class Register extends Component {
     }
   }
 
-  renderInput ({ type = 'text', name, placeholder, icon }) {
+  renderInput = ({ type = 'text', name, placeholder, icon }) => {
     return (
       <Form.Input type={type}
                   name={name}
@@ -38,6 +76,8 @@ export default class Register extends Component {
   }
 
   render () {
+    const { errors } = this.state
+
     return (
       <Grid className="app" textAlign="center" verticalAlign="middle">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -60,6 +100,11 @@ export default class Register extends Component {
 
               <Button color="orange" fluid size="large">Submit</Button>
             </Segment>
+
+            <Message error
+                     visible={errors.length > 0}
+                     header="There was some errors with your submission"
+                     list={errors.map((error) => error.message)}/>
 
             <Message>Already a user? <Link to="/login">Login</Link></Message>
           </Form>
