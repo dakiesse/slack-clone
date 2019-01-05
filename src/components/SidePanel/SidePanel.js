77524a882aac9a57dbfe2957786d89615a174b3c
Danil Chekalin <dakiesse@gmail.com>
2019-01-05 16:02:52 +0200

    makeup the dashboard

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+import UserPanel from '../UserPanel/UserPanel'
+
+class SidePanel extends Component {
+  render () {
+    return (
+      <Menu size="large" fixed="left" inverted vertical style={{ background: '#4c3c4c', fontSize: '1.2rem' }}>
+        <UserPanel></UserPanel>
+      </Menu>
+    )
+  }
+}
+
+export default SidePanel
---
+++ src/components/UserPanel/UserPanel.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Dropdown, Grid, Header, Icon } from 'semantic-ui-react'
+
+class UserPanel extends Component {
+  get dropdownOptions () {
+    return [
+      {
+        key: 'user',
+        text: <span>Signed in as <strong>User</strong></span>,
+        disabled: true,
+      },
+      {
+        key: 'avatar',
+
+        text: <span>Change Avatar</span>,
+      },
+      {
+        key: 'signout',
+        text: <span>Sign Out</span>,
+      },
+    ]
+  }
+
+  render () {
+    return (
+      <Grid style={{ background: '#4c3c4c' }}>
+        <Grid.Column>
+          <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
+            <Header floated="left" as="h2" inverted>
+              <Icon name="code"/>
+              <Header.Content>DevChat</Header.Content>
+            </Header>
+          </Grid.Row>
+
+          <Header as="h4" inverted style={{ padding: '.25em' }}>
+            <Dropdown trigger={
+              <span>User</span>
+            } options={this.dropdownOptions}/>
+          </Header>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
+
+export default UserPanel
