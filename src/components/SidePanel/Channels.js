3dc9bf3c5045a9ca5ed9fb32e6f7907a773e0b98
Danil Chekalin <dakiesse@gmail.com>
2019-01-06 02:27:20 +0200

    add modal for adding new channel

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,14 +1,78 @@
 import React, { Component } from 'react'
-import { Icon, Menu } from 'semantic-ui-react'
+import { Button, Form, Icon, Input, Menu, Modal } from 'semantic-ui-react'
 
 class Channels extends Component {
+  state = {
+    newChannelName: '',
+    newChannelDetails: '',
+    isOpeningModal: false,
+  }
+
+  handlerChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value })
+  }
+
+  openModal = () => {
+    this.setState({ isOpeningModal: true })
+  }
+
+  closeModal = () => {
+    this.setState({ isOpeningModal: false })
+  }
+
+  renderModal () {
+    const { isOpeningModal } = this.state
+
+    return (
+      <Modal open={isOpeningModal} onClose={this.closeModal} basic>
+        <Modal.Header>Add a Channel</Modal.Header>
+
+        <Modal.Content>
+          <Form>
+            <Form.Field>
+              <Input
+                label="Name of Channel"
+                name="newChannelName"
+                fluid
+                onChange={this.handlerChange}
+              />
+            </Form.Field>
+
+            <Form.Field>
+              <Input
+                label="About the Channel"
+                name="newChannelDetails"
+                fluid
+                onChange={this.handlerChange}
+              />
+            </Form.Field>
+          </Form>
+        </Modal.Content>
+
+        <Modal.Actions>
+          <Button color="green" inverted>
+            <Icon name="checkmark"/> Add
+          </Button>
+
+          <Button color="red" inverted onClick={this.closeModal}>
+            <Icon name="remove"/> Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    )
+  }
+
   render () {
     return (
-      <Menu.Menu style={{ paddingBottom: '2em' }}>
-        <Menu.Item>
-          <span><Icon name="exchange"/> CHANNELS</span> ({0}) <Icon name="add"/>
-        </Menu.Item>
-      </Menu.Menu>
+      <React.Fragment>
+        <Menu.Menu style={{ paddingBottom: '2em' }}>
+          <Menu.Item>
+            <span><Icon name="exchange"/> CHANNELS</span> ({0}) <Icon name="add" onClick={this.openModal}/>
+          </Menu.Item>
+        </Menu.Menu>
+
+        {this.renderModal()}
+      </React.Fragment>
     )
   }
 }
