a2a7937d7b411e0ff50e33bfece846e8b579f90c
Danil Chekalin <dakiesse@gmail.com>
2019-01-12 03:40:09 +0200

    add auto update for time messages

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,30 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Comment, Image } from 'semantic-ui-react'
 import moment from 'moment'
 
-const isOwnMessage = (message, user) => {
-  return message.user.id === user.uid ? 'message__self' : ''
-}
+const eachMilliseconds = 1000
 
-const timeFromNow = (timestamp) => {
-  return moment(timestamp).fromNow()
-}
+class Message extends Component {
+  timeoutId = null
 
-const isImage = (message) => {
-  return message.hasOwnProperty('image') && !message.hasOwnProperty('content')
-}
+  componentDidMount () {
+    this.updateRenderTime()
+  }
 
-const Message = ({ message, user }) => (
-  <Comment>
-    <Comment.Avatar src={message.user.avatar}/>
-    <Comment.Content className={isOwnMessage(message, user)}>
-      <Comment.Author as="a">{message.user.name}</Comment.Author>
-      <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
-      {isImage(message)
-        ? <Image className="message__image" src={message.image}/>
-        : <Comment.Text>{message.content}</Comment.Text>}
-    </Comment.Content>
-  </Comment>
-)
+  componentWillUnmount () {
+    clearTimeout(this.timeoutId)
+  }
+
+  updateRenderTime () {
+    const diff = Date.now() - this.props.message.timestamp
+    const delay = diff > 60000 ? 60000 : eachMilliseconds
+    console.log({ diff, delay })
+
+    this.timeoutId = setTimeout(() => {
+      this.forceUpdate()
+      this.updateRenderTime()
+    }, delay)
+  }
+
+  isOwnMessage (message, user) {
+    return message.user.id === user.uid ? 'message__self' : ''
+  }
+
+  timeFromNow (timestamp) {
+    return moment(timestamp).fromNow()
+  }
+
+  isImage (message) {
+    return message.hasOwnProperty('image') && !message.hasOwnProperty('content')
+  }
+
+  render () {
+    const { message, user } = this.props
+
+    return (
+      <Comment>
+        <Comment.Avatar src={message.user.avatar}/>
+        <Comment.Content className={this.isOwnMessage(message, user)}>
+          <Comment.Author as="a">{message.user.name}</Comment.Author>
+          <Comment.Metadata>{this.timeFromNow(message.timestamp)}</Comment.Metadata>
+          {this.isImage(message)
+            ? <Image className="message__image" src={message.image}/>
+            : <Comment.Text>{message.content}</Comment.Text>}
+        </Comment.Content>
+      </Comment>
+    )
+  }
+}
 
 export default Message
